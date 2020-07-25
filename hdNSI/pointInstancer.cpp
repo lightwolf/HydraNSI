lightwolf/HydraNSI

commit 8921881cb10acd7d983f3f3e2e750339bf09c93a

Rewrote instancer support.
- Now supports motion blur.
- Better support of nested instancers. In particular, primvars on parent
  instancers now work.
- NSI scene layout more closely matches what is in USD. Nested instancers are
  no longer flattened, all the transforms are where they belong, etc.
Requires 3Delight 2.1.12 if there are disabled instances.

hdNSI/pointInstancer.cpp
@@ -1,0 +1,347 @@
+#include "pointInstancer.h"
+
+#include "renderParam.h"
+#include "rprimBase.h"
+
+#include <pxr/base/gf/quatd.h>
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+#if defined(PXR_VERSION) && PXR_VERSION <= 1911
+/* Instancer tokens were not in 19.11 yet. So make them up. */
+TF_DEFINE_PRIVATE_TOKENS(
+	HdInstancerTokens,
+	(instanceTransform)
+	(scale)
+	(rotate)
+	(translate)
+);
+/* Same for this method of HdInstancer which returns them. */
+namespace
+{
+const TfTokenVector& GetBuiltinPrimvarNames()
+{
+	static const TfTokenVector primvarNames =
+	{
+		HdInstancerTokens->instanceTransform,
+		HdInstancerTokens->rotate,
+		HdInstancerTokens->scale,
+		HdInstancerTokens->translate
+	};
+	return primvarNames;
+}
+}
+#endif
+
+HdNSIPointInstancer::HdNSIPointInstancer(
+	HdSceneDelegate *sceneDelegate,
+	const SdfPath &id,
+	const SdfPath &parentInstancerId)
+:
+	HdInstancer{sceneDelegate, id, parentInstancerId},
+	m_primVars{false}
+{
+	/* Don't output the transform primvars as actual primvars. */
+	m_primVars.SetSkipVars(GetBuiltinPrimvarNames());
+}
+
+HdNSIPointInstancer::~HdNSIPointInstancer()
+{
+	assert(m_xformHandle.empty());
+	assert(m_instancerHandle.empty());
+}
+
+/**
+	\brief Remove the instancer from the NSI scene.
+*/
+void HdNSIPointInstancer::Destroy(
+	HdNSIRenderParam *renderParam)
+{
+	if (!m_instancerHandle.empty())
+	{
+		NSI::Context &nsi = renderParam->AcquireSceneForEdit();
+		nsi.Delete(m_instancerHandle);
+		nsi.Delete(m_xformHandle);
+		m_instancerHandle.clear();
+		m_xformHandle.clear();
+	};
+}
+
+/**
+	\brief Sync a prototype to this instancer.
+
+	\param renderParam
+		The render param.
+	\param prototypeId
+		The prototype's Id.
+	\param prototypeHandle
+		The NSI handle of the prototype's transform node.
+	\param isNewPrototype
+		true iff it is the first time this prototype does a Sync.
+*/
+void HdNSIPointInstancer::SyncPrototype(
+	HdNSIRenderParam *renderParam,
+	const SdfPath &prototypeId,
+	const std::string &prototypeHandle,
+	bool isNewPrototype)
+{
+	std::lock_guard<std::mutex> instancer_l(m_mutex);
+
+	HdRenderIndex &renderIndex = GetDelegate()->GetRenderIndex();
+	HdChangeTracker &changeTracker = renderIndex.GetChangeTracker();
+	const SdfPath &id = GetId();
+
+	NSI::Context &nsi = renderParam->AcquireSceneForEdit();
+	HdDirtyBits dirtyBits = changeTracker.GetInstancerDirtyBits(id);
+
+	if (m_instancerHandle.empty())
+	{
+		/* Create the instancer and its transform node. */
+		m_xformHandle = GetId().GetString();
+		m_instancerHandle = m_xformHandle + "|geo";
+		nsi.Create(m_xformHandle, "transform");
+		nsi.Create(m_instancerHandle, "instances");
+		nsi.Connect(m_instancerHandle, "", m_xformHandle, "objects");
+		const SdfPath &parent = GetParentId();
+		if (parent.IsEmpty())
+		{
+			/* No parent instancer. Add to the scene. */
+			nsi.Connect(m_xformHandle, "", NSI_SCENE_ROOT, "objects");
+		}
+		else
+		{
+			/* Add ourselves as a prototype for the parent instancer. */
+			auto instancer = static_cast<HdNSIPointInstancer*>(
+				renderIndex.GetInstancer(parent));
+			instancer->SyncPrototype(renderParam, id, m_xformHandle, true);
+		}
+	}
+
+	if (isNewPrototype )
+	{
+		/*
+			See if the prototype is already in the list. This could happen
+			because we never remove them from that list.
+		*/
+		unsigned pIdx;
+		for (pIdx = 0; pIdx < m_prototype_ids.size(); ++pIdx)
+		{
+			if (m_prototype_ids[pIdx] == prototypeId)
+				break;
+		}
+		/* If it's a new one, add it. */
+		if (pIdx == m_prototype_ids.size())
+		{
+			m_prototype_ids.emplace_back(prototypeId);
+		}
+
+		/* Connect it to the instancer. */
+		nsi.Connect(
+			prototypeHandle, "",
+			m_instancerHandle, "sourcemodels",
+			NSI::IntegerArg("index", pIdx));
+
+		/* Force refresh of model indices. */
+		dirtyBits |= HdChangeTracker::DirtyInstanceIndex;
+	}
+
+	/*
+		The code below will be run for each prototype. This is not ideal. We
+		should probably queue the dirty instancers somewhere and process them
+		after all the other Sync are done. There's a hook for that somewhere
+		but I forget exactly where.
+	*/
+
+	bool write_modelindices = false;
+	if (0 != (dirtyBits & HdChangeTracker::DirtyInstanceIndex))
+	{
+		/* Update model indices and instanceId */
+		m_prototype_indices.clear();
+		m_instance_id.clear();
+		for (size_t p = 0; p < m_prototype_ids.size(); ++p)
+		{
+			VtIntArray instanceIndices =
+				GetDelegate()->GetInstanceIndices(GetId(), m_prototype_ids[p]);
+			for (unsigned i = 0; i < instanceIndices.size(); ++i)
+			{
+				int idx = instanceIndices[i];
+				if (idx >= m_prototype_indices.size() )
+				{
+					/* Fill with -1 as there may be gaps in the final array if
+					   there are disabled instances. -1 will render nothing. */
+					m_prototype_indices.resize(idx + 1, -1);
+					/* The -1 here should not actually be used. */
+					m_instance_id.resize(idx + 1, -1);
+				}
+				m_prototype_indices[idx] = p;
+				m_instance_id[idx] = i;
+			}
+		}
+
+		/* Delay write a little as the array might still grow. */
+		write_modelindices = true;
+	}
+
+	/*
+		Do the instance xforms. These are primvars but excluded from the
+		generic primvar output as they need to be folded into a single matrix
+		per instance. They also need to come before generic primvars as those
+		clear the dirty bit.
+	*/
+	if (0 != (dirtyBits & HdChangeTracker::DirtyPrimvar))
+	{
+		/* First, fetch all the transform related primvars. */
+		TfToken xform_tokens[] =
+		{
+			HdInstancerTokens->instanceTransform,
+			HdInstancerTokens->scale,
+			HdInstancerTokens->rotate,
+			HdInstancerTokens->translate,
+		};
+		HdTimeSampleArray<VtValue, 4> xform_primvars[4];
+
+		unsigned num_transforms = 0;
+		TfSmallVector<float, 10> times;
+
+		HdPrimvarDescriptorVector primvars = GetDelegate()
+			->GetPrimvarDescriptors(id, HdInterpolationInstance);
+		for (const HdPrimvarDescriptor &primvar : primvars)
+		{
+			for (unsigned i = 0; i < 4; ++i)
+			{
+				if (primvar.name == xform_tokens[i])
+				{
+					auto &samples = xform_primvars[i];
+					GetDelegate()->SamplePrimvar(id, primvar.name, &samples);
+					times.insert(times.end(),
+						samples.times.data(),
+						samples.times.data() + samples.count);
+					if (samples.count != 0)
+						num_transforms = samples.values[0].GetArraySize();
+				}
+			}
+		}
+
+		/* Compute set of unique time samples. */
+		std::sort(times.begin(), times.end());
+		times.erase(std::unique(times.begin(), times.end()), times.end());
+
+		if (times.size() > 1)
+		{
+			/* Delete previous time samples. */
+			nsi.DeleteAttribute(m_instancerHandle, "transformationmatrices");
+		}
+
+		/* Unbox the values from VtValue to actual type. */
+		HdTimeSampleArray<VtMatrix4dArray, 4> pv_transform;
+		HdTimeSampleArray<VtVec3fArray, 4> pv_scale;
+		HdTimeSampleArray<VtVec4fArray, 4> pv_rotate;
+		HdTimeSampleArray<VtVec3fArray, 4> pv_translate;
+		pv_transform.UnboxFrom(xform_primvars[0]);
+		pv_scale.UnboxFrom(xform_primvars[1]);
+		pv_rotate.UnboxFrom(xform_primvars[2]);
+		pv_translate.UnboxFrom(xform_primvars[3]);
+
+		/* For each time sample, concatenate the vars into a single matrix. */
+		VtMatrix4dArray transforms{num_transforms};
+		for (float t : times)
+		{
+			/* Start with a bunch of identity matrices. */
+			std::fill_n(transforms.begin(), num_transforms, GfMatrix4d(1.0));
+			GfMatrix4d m;
+			/* Apply instanceTransform. */
+			if (pv_transform.count != 0)
+			{
+				VtMatrix4dArray values = pv_transform.Resample(t);
+				for (unsigned i = 0; i < num_transforms; ++i)
+				{
+					transforms[i] *= values[i];
+				}
+			}
+			/* Apply scale. */
+			if (pv_scale.count != 0)
+			{
+				VtVec3fArray values = pv_scale.Resample(t);
+				for (unsigned i = 0; i < num_transforms; ++i)
+				{
+					transforms[i] *= m.SetScale(values[i]);
+				}
+			}
+			/* Apply rotate. */
+			if (pv_rotate.count != 0)
+			{
+				VtVec4fArray values = pv_rotate.Resample(t);
+				for (unsigned i = 0; i < num_transforms; ++i)
+				{
+					auto v = values[i];
+					transforms[i] *= m.SetRotate(
+						GfQuatd(v[0], v[1], v[2], v[3]));
+				}
+			}
+			/* Apply translate. */
+			if (pv_translate.count != 0)
+			{
+				VtVec3fArray values = pv_translate.Resample(t);
+				for (unsigned i = 0; i < num_transforms; ++i)
+				{
+					transforms[i] *= m.SetTranslate(GfVec3d(values[i]));
+				}
+			}
+
+			NSI::Argument arg("transformationmatrices");
+			arg.SetType(NSITypeDoubleMatrix);
+			arg.SetCount(transforms.size());
+			arg.SetValuePointer(transforms.data());
+			if (times.size() > 1)
+			{
+				nsi.SetAttributeAtTime(m_instancerHandle, t, arg);
+			}
+			else
+			{
+				nsi.SetAttribute(m_instancerHandle, arg);
+			}
+		}
+
+		/* If the last instance if disabled, the indices array will be too
+		   short. Enlarge it here. */
+		if (m_prototype_indices.size() < num_transforms)
+		{
+			m_prototype_indices.resize(num_transforms, -1);
+			m_instance_id.resize(num_transforms, -1);
+			write_modelindices = true;
+		}
+	}
+
+	if (write_modelindices)
+	{
+		nsi.SetAttribute(m_instancerHandle,
+			*NSI::Argument("modelindices")
+			.SetType(NSITypeInteger)
+			->SetCount(m_prototype_indices.size())
+			->SetValuePointer(m_prototype_indices.data()));
+
+		/* This is for the instanceId AOV. */
+		nsi.SetAttribute(m_instancerHandle,
+			*NSI::Argument("instanceId")
+			.SetType(NSITypeInteger)
+			->SetCount(m_instance_id.size())
+			->SetValuePointer(m_instance_id.data()));
+	}
+
+	/* This handles the single instancer transform. */
+	if (0 != (dirtyBits & HdChangeTracker::DirtyTransform))
+	{
+		HdNSIRprimBase::ExportTransform(
+			GetDelegate(), id, true, nsi, m_xformHandle);
+	}
+
+	/* Do the primvars. */
+	m_primVars.Sync(
+		GetDelegate(), renderParam, &dirtyBits, nsi, id, m_instancerHandle, {});
+
+	/* Mark instancer as clean. */
+	changeTracker.MarkInstancerClean(id);
+}
+
+PXR_NAMESPACE_CLOSE_SCOPE
+// vim: set softtabstop=0 noexpandtab shiftwidth=4:

hdNSI/CMakeLists.txt
@@ -19,7 +19,6 @@ add_library(
 	curves.cpp
 	discoveryPlugin.cpp
 	field.cpp
-	instancer.cpp
 	light.cpp
 	materialAssign.cpp
 	material.cpp
@@ -27,6 +26,7 @@ add_library(
 	osoParserPlugin.cpp
 	outputDriver.cpp
 	pointcloud.cpp
+	pointInstancer.cpp
 	primvars.cpp
 	renderBuffer.cpp
 	renderDelegate.cpp

hdNSI/curves.cpp
@@ -26,7 +26,6 @@
 
 #include "curves.h"
 
-#include "instancer.h"
 #include "renderDelegate.h"
 #include "renderParam.h"
 #include "renderPass.h"

hdNSI/instancer.cpp
@@ -1,313 +1,0 @@
-//
-// Copyright 2016 Pixar
-// Copyright 2018 Illumination Research Pte Ltd.
-// Authors: J Cube Inc (Marco Pantaleoni, Bo Zhou, Paolo Berto Durante)
-//
-// Licensed under the Apache License, Version 2.0 (the "Apache License")
-// with the following modification; you may not use this file except in
-// compliance with the Apache License and the following modification to it:
-// Section 6. Trademarks. is deleted and replaced with:
-//
-// 6. Trademarks. This License does not grant permission to use the trade
-//    names, trademarks, service marks, or product names of the Licensor
-//    and its affiliates, except as required to comply with Section 4(c) of
-//    the License and to reproduce the content of the NOTICE file.
-//
-// You may obtain a copy of the Apache License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the Apache License with the above modification is
-// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied. See the Apache License for the specific
-// language governing permissions and limitations under the Apache License.
-//
-
-#include "instancer.h"
-
-#include "primvars.h"
-
-#include <pxr/base/gf/vec2f.h>
-#include <pxr/base/gf/vec3f.h>
-#include <pxr/base/gf/vec4f.h>
-#include <pxr/base/gf/matrix4d.h>
-#include <pxr/base/gf/rotation.h>
-#include <pxr/base/gf/quaternion.h>
-#include <pxr/base/tf/staticTokens.h>
-
-PXR_NAMESPACE_OPEN_SCOPE
-
-// Define local tokens for the names of the primvars the instancer
-// consumes.
-// XXX: These should be hydra tokens...
-TF_DEFINE_PRIVATE_TOKENS(
-    _tokens,
-    (instanceTransform)
-    (rotate)
-    (scale)
-    (translate)
-);
-
-HdNSIInstancer::HdNSIInstancer(HdSceneDelegate* delegate,
-                                     SdfPath const& id,
-                                     SdfPath const &parentId)
-    : HdInstancer(delegate, id, parentId)
-{
-}
-
-HdNSIInstancer::~HdNSIInstancer()
-{
-}
-
-void
-HdNSIInstancer::_SyncPrimvars()
-{
-    HD_TRACE_FUNCTION();
-    HF_MALLOC_TAG_FUNCTION();
-
-    HdChangeTracker &changeTracker = 
-        GetDelegate()->GetRenderIndex().GetChangeTracker();
-    SdfPath const& id = GetId();
-
-    // Use the double-checked locking pattern to check if this instancer's
-    // primvars are dirty.
-    int dirtyBits = changeTracker.GetInstancerDirtyBits(id);
-    if (HdChangeTracker::IsAnyPrimvarDirty(dirtyBits, id)) {
-        std::lock_guard<std::mutex> lock(_instanceLock);
-
-        dirtyBits = changeTracker.GetInstancerDirtyBits(id);
-        if (HdChangeTracker::IsAnyPrimvarDirty(dirtyBits, id)) {
-
-            // If this instancer has dirty primvars, get the list of
-            // primvar names and then cache each one.
-
-            TfTokenVector primvarNames;
-            HdPrimvarDescriptorVector primvars = GetDelegate()
-                ->GetPrimvarDescriptors(id, HdInterpolationInstance);
-
-            for (HdPrimvarDescriptor const& pv: primvars) {
-                if (HdChangeTracker::IsPrimvarDirty(dirtyBits, id, pv.name)) {
-                    VtValue value = GetDelegate()->Get(id, pv.name);
-                    if (!value.IsEmpty())
-                    {
-                        CachedPv cpv;
-                        cpv.descriptor = pv;
-                        cpv.value = value;
-                        _primvarMap[pv.name] = cpv;
-                    }
-                }
-            }
-
-            // Mark the instancer as clean
-            changeTracker.MarkInstancerClean(id);
-        }
-    }
-}
-
-VtMatrix4dArray
-HdNSIInstancer::ComputeInstanceTransforms(SdfPath const &prototypeId)
-{
-    HD_TRACE_FUNCTION();
-    HF_MALLOC_TAG_FUNCTION();
-
-    _SyncPrimvars();
-
-    // The transforms for this level of instancer are computed by:
-    // foreach(index : indices) {
-    //     instancerTransform * translate(index) * rotate(index) *
-    //     scale(index) * instanceTransform(index)
-    // }
-    // If any transform isn't provided, it's assumed to be the identity.
-
-    GfMatrix4d instancerTransform =
-        GetDelegate()->GetInstancerTransform(GetId());
-    VtIntArray instanceIndices =
-        GetDelegate()->GetInstanceIndices(GetId(), prototypeId);
-
-    VtMatrix4dArray transforms(instanceIndices.size());
-    for (size_t i = 0; i < instanceIndices.size(); ++i) {
-        transforms[i] = instancerTransform;
-    }
-
-    // "translate" holds a translation vector for each index.
-    if (_primvarMap.count(_tokens->translate) > 0)
-    {
-        const VtVec3fArray values =
-            _primvarMap[_tokens->translate].value.Get<VtVec3fArray>();
-        for (size_t i = 0; i < instanceIndices.size(); ++i)
-        {
-            if (size_t(instanceIndices[i]) < values.size())
-            {
-                const auto &v = values[instanceIndices[i]];
-                GfMatrix4d translateMat(1);
-                translateMat.SetTranslate(GfVec3d(v));
-                transforms[i] = translateMat * transforms[i];
-            }
-        }
-    }
-
-    // "rotate" holds a quaternion in <real, i, j, k> format for each index.
-    if (_primvarMap.count(_tokens->rotate) > 0)
-    {
-        const VtVec4fArray values =
-            _primvarMap[_tokens->rotate].value.Get<VtVec4fArray>();
-        for (size_t i = 0; i < instanceIndices.size(); ++i)
-        {
-            if (size_t(instanceIndices[i]) < values.size())
-            {
-                const auto &v = values[instanceIndices[i]];
-                GfMatrix4d rotateMat(1);
-                rotateMat.SetRotate(GfRotation(GfQuaternion(
-                    v[0], GfVec3d(v[1], v[2], v[3]))));
-                transforms[i] = rotateMat * transforms[i];
-            }
-        }
-    }
-
-    // "scale" holds an axis-aligned scale vector for each index.
-    if (_primvarMap.count(_tokens->scale) > 0)
-    {
-        const VtVec3fArray values =
-            _primvarMap[_tokens->scale].value.Get<VtVec3fArray>();
-        for (size_t i = 0; i < instanceIndices.size(); ++i)
-        {
-            if (size_t(instanceIndices[i]) < values.size())
-            {
-                const auto &v = values[instanceIndices[i]];
-                GfMatrix4d scaleMat(1);
-                scaleMat.SetScale(GfVec3d(v));
-                transforms[i] = scaleMat * transforms[i];
-            }
-        }
-    }
-
-    // "instanceTransform" holds a 4x4 transform matrix for each index.
-    if (_primvarMap.count(_tokens->instanceTransform) > 0)
-    {
-        const VtMatrix4dArray values =
-            _primvarMap[_tokens->instanceTransform].value
-            .Get<VtMatrix4dArray>();
-        for (size_t i = 0; i < instanceIndices.size(); ++i)
-        {
-            if (size_t(instanceIndices[i]) < values.size())
-            {
-                const auto &v = values[instanceIndices[i]];
-                transforms[i] = v * transforms[i];
-            }
-        }
-    }
-
-    if (GetParentId().IsEmpty()) {
-        return transforms;
-    }
-
-    HdInstancer *parentInstancer =
-        GetDelegate()->GetRenderIndex().GetInstancer(GetParentId());
-    if (!TF_VERIFY(parentInstancer)) {
-        return transforms;
-    }
-
-    // The transforms taking nesting into account are computed by:
-    // parentTransforms = parentInstancer->ComputeInstanceTransforms(GetId())
-    // foreach (parentXf : parentTransforms, xf : transforms) {
-    //     parentXf * xf
-    // }
-    VtMatrix4dArray parentTransforms =
-        static_cast<HdNSIInstancer*>(parentInstancer)->
-            ComputeInstanceTransforms(GetId());
-
-    VtMatrix4dArray final(parentTransforms.size() * transforms.size());
-    for (size_t i = 0; i < parentTransforms.size(); ++i) {
-        for (size_t j = 0; j < transforms.size(); ++j) {
-            final[i * transforms.size() + j] = transforms[j] *
-                                               parentTransforms[i];
-        }
-    }
-    return final;
-}
-
-/*
-    Select some items from an array according to indices in another array.
-*/
-template<typename ArrayT>
-ArrayT SelectArrayItems(const VtIntArray indices, const ArrayT data)
-{
-    ArrayT newData;
-    newData.resize(indices.size());
-    for (size_t i = 0; i < indices.size(); ++i)
-    {
-        if (size_t(indices[i]) < data.size())
-        {
-            newData[i] = data[indices[i]];
-        }
-    }
-    return newData;
-}
-
-/*
-    This exports all instance primvars which are not transform related, for a
-    given prototype.
-*/
-void HdNSIInstancer::ExportInstancePrimvars(
-    const SdfPath &prototypeId,
-	HdNSIRenderParam *renderParam,
-    const std::string &instancesHandle)
-{
-    HD_TRACE_FUNCTION();
-    HF_MALLOC_TAG_FUNCTION();
-
-    _SyncPrimvars();
-
-    VtIntArray indices =
-        GetDelegate()->GetInstanceIndices(GetId(), prototypeId);
-
-    for (const auto &item : _primvarMap)
-    {
-        /* Skip the transforms which are processed separately. */
-        if (item.first == _tokens->translate ||
-            item.first == _tokens->rotate ||
-            item.first == _tokens->scale ||
-            item.first == _tokens->instanceTransform )
-        {
-            continue;
-        }
-
-        VtValue v = item.second.value;
-        if (v.IsEmpty())
-            continue;
-
-	    NSI::Context &nsi = renderParam->AcquireSceneForEdit();
-        VtValue newv;
-        if (v.IsHolding<VtArray<TfToken>>())
-        {
-            newv = SelectArrayItems(indices, v.Get<VtArray<TfToken>>());
-        }
-        else if (v.IsHolding<VtArray<std::string>>())
-        {
-            newv = SelectArrayItems(indices, v.Get<VtArray<std::string>>());
-        }
-        else if (v.IsHolding<VtArray<float>>())
-        {
-            newv = SelectArrayItems(indices, v.Get<VtArray<float>>());
-        }
-        else if (v.IsHolding<VtArray<GfVec2f>>())
-        {
-            newv = SelectArrayItems(indices, v.Get<VtArray<GfVec2f>>());
-        }
-        else if (v.IsHolding<VtArray<GfVec3f>>())
-        {
-            newv = SelectArrayItems(indices, v.Get<VtArray<GfVec3f>>());
-        }
-        else if (v.IsHolding<VtArray<int>>())
-        {
-            newv = SelectArrayItems(indices, v.Get<VtArray<int>>());
-        }
-
-        HdNSIPrimvars::SetAttributeFromValue(
-            nsi, instancesHandle, item.second.descriptor, newv, 0, 0.0, false);
-    }
-}
-
-PXR_NAMESPACE_CLOSE_SCOPE
-// vim: set softtabstop=4 expandtab shiftwidth=4:

hdNSI/instancer.h
@@ -1,112 +1,0 @@
-//
-// Copyright 2016 Pixar
-// Copyright 2018 Illumination Research Pte Ltd.
-// Authors: J Cube Inc (Marco Pantaleoni, Bo Zhou, Paolo Berto Durante)
-//
-// Licensed under the Apache License, Version 2.0 (the "Apache License")
-// with the following modification; you may not use this file except in
-// compliance with the Apache License and the following modification to it:
-// Section 6. Trademarks. is deleted and replaced with:
-//
-// 6. Trademarks. This License does not grant permission to use the trade
-//    names, trademarks, service marks, or product names of the Licensor
-//    and its affiliates, except as required to comply with Section 4(c) of
-//    the License and to reproduce the content of the NOTICE file.
-//
-// You may obtain a copy of the Apache License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the Apache License with the above modification is
-// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied. See the Apache License for the specific
-// language governing permissions and limitations under the Apache License.
-//
-#ifndef HDNSI_INSTANCER_H
-#define HDNSI_INSTANCER_H
-
-#include "renderParam.h"
-
-#include <pxr/base/tf/hashmap.h>
-#include <pxr/base/tf/token.h>
-#include <pxr/base/vt/value.h>
-#include <pxr/imaging/hd/instancer.h>
-#include <pxr/imaging/hd/sceneDelegate.h>
-#include <pxr/pxr.h>
-
-#include <mutex>
-
-PXR_NAMESPACE_OPEN_SCOPE
-
-/// \class HdNSIInstancer
-///
-/// HdNSI implements instancing by adding prototype geometry to the BVH
-/// multiple times within HdNSIMesh::Sync(). The only instance-varying
-/// attribute that HdNSI supports is transform, so the natural
-/// accessor to instancer data is ComputeInstanceTransforms(),
-/// which returns a list of transforms to apply to the given prototype
-/// (one instance per transform).
-///
-/// Nested instancing can be handled by recursion, and by taking the
-/// cartesian product of the transform arrays at each nesting level, to
-/// create a flattened transform array.
-///
-class HdNSIInstancer : public HdInstancer {
-public:
-    /// Constructor.
-    ///   \param delegate The scene delegate backing this instancer's data.
-    ///   \param id The unique id of this instancer.
-    ///   \param parentInstancerId The unique id of the parent instancer,
-    ///                            or an empty id if not applicable.
-    HdNSIInstancer(HdSceneDelegate* delegate, SdfPath const& id,
-                      SdfPath const &parentInstancerId);
-
-    /// Destructor.
-    ~HdNSIInstancer();
-
-    /// Computes all instance transforms for the provided prototype id,
-    /// taking into account the scene delegate's instancerTransform and the
-    /// instance primvars "instanceTransform", "translate", "rotate", "scale".
-    /// Computes and flattens nested transforms, if necessary.
-    ///   \param prototypeId The prototype to compute transforms for.
-    ///   \return One transform per instance, to apply when drawing.
-    VtMatrix4dArray ComputeInstanceTransforms(SdfPath const &prototypeId);
-
-    void ExportInstancePrimvars(
-        const SdfPath &prototypeId,
-		HdNSIRenderParam *renderParam,
-        const std::string &instancesHandle);
-
-private:
-    // Checks the change tracker to determine whether instance primvars are
-    // dirty, and if so pulls them. Since primvars can only be pulled once,
-    // and are cached, this function is not re-entrant. However, this function
-    // is called by ComputeInstanceTransforms, which is called (potentially)
-    // by HdNSIMesh::Sync(), which is dispatched in parallel, so it needs
-    // to be guarded by _instanceLock.
-    //
-    // Pulled primvars are cached in _primvarMap.
-    void _SyncPrimvars();
-
-    // Mutex guard for _SyncPrimvars().
-    std::mutex _instanceLock;
-
-    struct CachedPv
-    {
-        HdPrimvarDescriptor descriptor;
-        VtValue value;
-    };
-
-    // Map of the latest primvar data for this instancer, keyed by
-    // primvar name.
-    TfHashMap<TfToken,
-              CachedPv,
-              TfToken::HashFunctor> _primvarMap;
-};
-
-
-PXR_NAMESPACE_CLOSE_SCOPE
-
-#endif  // HDNSI_INSTANCER_H
-// vim: set softtabstop=4 expandtab shiftwidth=4:

hdNSI/light.cpp
@@ -44,7 +44,7 @@ void HdNSILight::Sync(
 	if (0 != (*dirtyBits & DirtyTransform))
 	{
 		HdNSIRprimBase::ExportTransform(
-			sceneDelegate, GetId(), nsi, xform_handle);
+			sceneDelegate, GetId(), false, nsi, xform_handle);
 	}
 
 	if (0 != (*dirtyBits & DirtyParams))

hdNSI/pointInstancer.h
@@ -1,0 +1,71 @@
+#ifndef HDNSI_POINTINSTANCER_H
+#define HDNSI_POINTINSTANCER_H
+
+#include "primvars.h"
+
+#include <pxr/imaging/hd/instancer.h>
+#include <pxr/imaging/hd/sceneDelegate.h>
+
+#include <mutex>
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+class HdNSIRenderParam;
+
+/*
+	This class handles point instancer primitives.
+
+	The way this works in Hydra is a little awkward. The instancers are not
+	first class primitives (eg. they don't have Sync() and Finalize() methods).
+	Rather, they act as a sort of common point of communication for the
+	multiple prototype primitives which might use said instancer. This is
+	likely another kludge inherited from the needs of the GL renderer.
+
+	The way we handle this is what we progressively build the list of models as
+	we become aware of the prototype primitives which use a given instancer. It
+	may waste some space by going through some intermediate states but it makes
+	other things (eg. primvar export) simpler to have a single instancer with
+	all the primitives.
+*/
+class HdNSIPointInstancer : public HdInstancer
+{
+public:
+	HdNSIPointInstancer(
+		HdSceneDelegate *sceneDelegate,
+		const SdfPath &id,
+		const SdfPath &parentInstancerId);
+
+	~HdNSIPointInstancer() override;
+
+	void Destroy(
+		HdNSIRenderParam *renderParam);
+
+	void SyncPrototype(
+		HdNSIRenderParam *renderParam,
+		const SdfPath &prototypeId,
+		const std::string &prototypeHandle,
+		bool isNewPrototype);
+
+private:
+	/* Handle of transform node. */
+	std::string m_xformHandle;
+	/* Handle of instancer node. */
+	std::string m_instancerHandle;
+	/* List of prototypes using this instancer. */
+	std::vector<SdfPath> m_prototype_ids;
+	/* Index of the prototype used by each instance. */
+	std::vector<int> m_prototype_indices;
+	/* The instanceId attribute (for AOV). */
+	std::vector<int> m_instance_id;
+
+	HdNSIPrimvars m_primVars;
+
+	/* Because this can be used by multiple prototypes in ||, we need some
+	   locking. */
+	std::mutex m_mutex;
+};
+
+PXR_NAMESPACE_CLOSE_SCOPE
+
+#endif
+// vim: set softtabstop=0 noexpandtab shiftwidth=4:

hdNSI/pointcloud.cpp
@@ -26,7 +26,6 @@
 
 #include "pointcloud.h"
 
-#include "instancer.h"
 #include "renderDelegate.h"
 #include "renderParam.h"
 #include "renderPass.h"

commit 866b7afbaa3cd72e0668b39a9e5c0f15907ad690

Fix point instancer rotation support.
It receives GfQuath, not GfVec4f. This was broken by 8921881cb1. The
confusion came from the fact that it is GfVec4f when we "Get" the
primvar but GfQuath when we "Sample" the primvar. What a mess.

hdNSI/pointInstancer.cpp
@@ -4,6 +4,7 @@
 #include "rprimBase.h"
 
 #include <pxr/base/gf/quatd.h>
+#include <pxr/base/gf/quath.h>
 
 PXR_NAMESPACE_OPEN_SCOPE
 
@@ -235,7 +236,7 @@ void HdNSIPointInstancer::SyncPrototype(
 		/* Unbox the values from VtValue to actual type. */
 		HdTimeSampleArray<VtMatrix4dArray, 4> pv_transform;
 		HdTimeSampleArray<VtVec3fArray, 4> pv_scale;
-		HdTimeSampleArray<VtVec4fArray, 4> pv_rotate;
+		HdTimeSampleArray<VtQuathArray, 4> pv_rotate;
 		HdTimeSampleArray<VtVec3fArray, 4> pv_translate;
 		pv_transform.UnboxFrom(xform_primvars[0]);
 		pv_scale.UnboxFrom(xform_primvars[1]);
@@ -270,12 +271,11 @@ void HdNSIPointInstancer::SyncPrototype(
 			/* Apply rotate. */
 			if (pv_rotate.count != 0)
 			{
-				VtVec4fArray values = pv_rotate.Resample(t);
+				VtQuathArray values = pv_rotate.Resample(t);
 				for (unsigned i = 0; i < num_transforms; ++i)
 				{
 					auto v = values[i];
-					transforms[i] *= m.SetRotate(
-						GfQuatd(v[0], v[1], v[2], v[3]));
+					transforms[i] *= m.SetRotate(v);
 				}
 			}
 			/* Apply translate. */